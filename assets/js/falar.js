abdd0af5986eb9145218fdf8b26dfcd48f8b5b51
brunof1 <bhpdownloads@gmail.com>
2025-06-20 22:45:45 -0300

    0.1
    Versão Inicial

--- assets/js/falar.js
+++ assets/js/falar.js
@@ -3,3 +3,19 @@ function falar(texto) {
     const utter = new SpeechSynthesisUtterance(texto);
     synth.speak(utter);
 }
+
+function falarListaDeCartoes(listaTextos) {
+    let synth = window.speechSynthesis;
+    let indice = 0;
+
+    function falarProximo() {
+        if (indice < listaTextos.length) {
+            let utter = new SpeechSynthesisUtterance(listaTextos[indice]);
+            utter.onend = falarProximo;
+            synth.speak(utter);
+            indice++;
+        }
+    }
+
+    falarProximo();
+}
